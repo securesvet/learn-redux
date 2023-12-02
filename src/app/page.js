8252bc9f8697a8c274cde5863935624f0ae05637
Svet <mursvyats@gmail.com>
2023-12-02 23:57:23 +0500

    init

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center w-screen h-screen">
+      <h1>Hello world!</h1>
+    </main>
+  )
+}
